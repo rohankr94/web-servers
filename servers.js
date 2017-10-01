e5384d8722a2b6411b0db182c74c219ecfba5e2a
Rohan Kumar <rohankumar1800@gmail.com>
2017-10-01 15:39:11 +0530

    Initial commit

---
+++ servers.js
@@ -0,0 +1,44 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getFullYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(str) => {
+  return str.toUpperCase();
+});
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log=`${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n');
+  next();
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintainance.hbs',{
+//     message : 'Oh snap ! site under maintainance'
+//   })
+// });
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle : 'About page',
+  });
+});
+
+app.get('/',(req,res) => {
+  res.send({
+    page : 'Homepage',
+    number : '123546'
+  });
+});
+app.listen(3000);
